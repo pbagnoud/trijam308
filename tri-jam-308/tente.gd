2904149d00359d767a25fe652d59551f7b94d57f
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-08 16:38:58 +0100

    tente logic and shape

---
+++ tri-jam-308/tente.gd
@@ -0,0 +1,16 @@
+extends Node2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var objet_disponible: bool = true:
+	set(value):
+		objet_disponible = value
+		sprit_2d.modulate = 666666
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
