223fc464843f6da44011205184e421b5fdc954ce
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-10 21:33:28 +0100

    Update sortie.gd

--- tri-jam-308/sortie.gd
+++ tri-jam-308/sortie.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 @onready var fee_text: RichTextLabel = $FeeText
 
+var body_proximal = 'no'
+
 func _process(delta: float) -> void:
+	if body_proximal == 'no':
+		pass
 	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -14,3 +18,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		fee_text.visible = false
+		body_proximal = 'no'
