8201d31c53b7b2ed64ef103c055f7e4e00cac2ba
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-10 22:00:11 +0100

    sortie

--- tri-jam-308/sortie.gd
+++ tri-jam-308/sortie.gd
@@ -4,9 +4,26 @@ extends Node2D
 
 var body_proximal = 'no'
 
+func distance_to_proximity(distance: float) -> float:
+	var max_distance = 100.0
+	var min_distance = 10.0
+
+	if distance <= min_distance:
+		return 1
+	elif distance >= max_distance:
+		return 0.0
+	else:
+		var x = (distance - min_distance) / (max_distance - min_distance)
+		var proximity = 1 / (1.0 + exp((x - 0.5) * 10.0))
+		return proximity
+
+
 func _process(delta: float) -> void:
 	if body_proximal == 'no':
 		pass
+	else:
+		var distance = position.distance_to(body_proximal.position)
+		fee_text.modulate = Color(distance_to_proximity(distance),0,0,1)
 
 	
 
