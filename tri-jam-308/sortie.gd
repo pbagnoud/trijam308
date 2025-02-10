178b9f2c65a11fc235eb2ee93c1b0e11961230a5
PB <119977776+pbagnoud@users.noreply.github.com>
2025-02-10 21:22:26 +0100

    Add an asset in sortie

--- tri-jam-308/sortie.gd
+++ tri-jam-308/sortie.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
+@onready var fee_text: RichTextLabel = $FeeText
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group('player'):
+		if body.get_money() == 0:
+			fee_text.visible = true
+			
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group('player'):
+		fee_text.visible = false
